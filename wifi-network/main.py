fe5396429e3518b80dc4d043e3b6ec43a9627007
manfred michael <koskosanrider@gmail.com>
2022-03-15 16:26:36 +0700

    Solve testcase solution

--- wifi-network/main.py
+++ wifi-network/main.py
@@ -17,8 +17,8 @@ def timing(f):
 
 class Engineer:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = int(x)
+        self.y = int(y)
         self.signal = 0
 
     def is_inside(self, hub, r):
@@ -44,18 +44,27 @@ class SignalChecker:
 def get_input(filename):
     with open(filename, 'r') as f:
         lines = [line.strip() for line in f.readlines()]
-    print(lines)
     n_engineer = int(lines[0])
     n_query = int(lines[n_engineer+2])
-    coor_engineers = [tuple(line.split(' ')) for line in lines[1:1+n_engineer]]
-    range_query = [tuple(line.split(' ')) for line in lines[n_engineer+3:n_engineer+3+n_query]]
-    coor_hubs = lines[n_engineer+1].split(' ')
+    coor_engineers = [(int(line.split(' ')[0]), int(line.split(' ')[1])) for line in lines[1:1+n_engineer]]
+    range_query = [(int(line.split(' ')[0]), int(line.split(' ')[1]))for line in lines[n_engineer+3:n_engineer+3+n_query]]
+    coor_hubs = [int(coor) for coor in lines[n_engineer+1].split(' ')]
     return coor_hubs, coor_engineers, range_query
 
 def main():
     coor_hubs, coor_engineers, range_query = get_input('testcase')
+    Cg = Hub(coor_hubs[0], coor_hubs[1])
+    Ca = Hub(coor_hubs[2], coor_hubs[3])
+    engineers = [Engineer(x, y) for x, y in coor_engineers]
+    for Rg, Ra in range_query:
+        for engineer in engineers:
+            engineer.add_signal(Cg, Rg)
+            engineer.add_signal(Ca, Ra)
+        result = SignalChecker.get_nosignal_engineers(engineers)
+        print(result)
+        for engineer in engineers:
+            engineer.reset()
 
-    print(result)
 
 if __name__=='__main__':
     main()
